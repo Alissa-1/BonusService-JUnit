373d66da5a0b95ec24c17e57920632e2678d9169
Alissa-1 <alena.cheremukhina@gmail.com>
2022-05-30 13:51:08 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        System.out.println(service.calculate(8100, true));
+    }
+}
