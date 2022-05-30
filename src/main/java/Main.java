a7b0c70f4960c8d0dd291a3d316925c3e4b73a92
Alissa-1 <103644741+Alissa-1@users.noreply.github.com>
2022-05-30 13:52:50 +0300

    Delete Main.java

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BonusService service = new BonusService();
-        System.out.println(service.calculate(8100, true));
-    }
-}
